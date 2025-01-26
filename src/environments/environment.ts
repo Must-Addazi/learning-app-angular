1d00aba6d7eb7d7db1b2d92d1e84bfb646f848c5
Mustapha Addazi <addazimustapha123@gmail.com>
2025-01-26 10:52:41 +0100

    deploy backend in railway

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
     production: false,
     //backendHost:"http://localhost:8080"
-     backendHost: "https://addazimustaphaensas.onrender.com"
+    // backendHost: "https://addazimustaphaensas.onrender.com"
+     backendHost: "https://learning-app-spring-production.up.railway.app"
+
   };
   
